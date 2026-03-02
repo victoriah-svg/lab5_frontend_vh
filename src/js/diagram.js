5a6eeac7d4bb0711ad6eb5029959e82816ef0143
Victoria Helin <vickanhelin@hotmail.com>
2026-03-02 16:18:15 +0100

    lagt till async funktion som hämtar in kurser från antagningsstatistik på diagramsidan

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,5 +1,30 @@
 "use strict";
 
+const url = `https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json`;
+
+document.addEventListener("DOMContentLoaded", async () => {
+    loadCourses();
+});
+
+async function loadCourses() {
+    
+
+    try {
+        const response = await fetch(url);
+        const courses = await response.json();
+        // lagrar alla kurser i global array
+       let allCourses = courses;
+       allCourses.forEach(course => {
+        console.log(course.name, course.applicantsTotal);
+       });
+        
+
+    }
+    catch (error) {
+        console.error(`Felmeddelande: ${error}`);
+    }
+}
+
 /* för Chart.js */
 
 const chartDiv = document.getElementById("myChart");
